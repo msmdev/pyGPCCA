bcc90149d6b06412c3363213b030bb625f7e86bd
michalk8 <michal.klein@protonmail.com>
2021-01-14 15:19:09 +0100

    Use force and verbose

--- .scripts/ci/symlink_numpy.sh
+++ .scripts/ci/symlink_numpy.sh
@@ -5,12 +5,6 @@ set -euo pipefail
 pip install numpy
 
 NUMPY_INCLUDE="$(python -c 'import numpy; print(numpy.get_include())')"
-LINKNAME="$PETSC_DIR/$PETSC_ARCH/include"
-
 echo "Numpy header files location: $NUMPY_INCLUDE"
 
-if [[ -L "$LINKNAME" ]]; then
-    unlink "$LINKNAME"
-fi
-
-ln -s "$NUMPY_INCLUDE/numpy" "$LINKNAME"
+ln -sfv "$NUMPY_INCLUDE/numpy" "$PETSC_DIR/$PETSC_ARCH/include"
