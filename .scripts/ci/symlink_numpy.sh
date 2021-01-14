cef937bdc1519ade1c24f75de38a21e4b5d50f7f
michalk8 <michal.klein@protonmail.com>
2021-01-14 15:07:35 +0100

    Fix typo

--- .scripts/ci/symlink_numpy.sh
+++ .scripts/ci/symlink_numpy.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 pip install numpy
 
 NUMPY_INCLUDE="$(python -c 'import numpy; print(numpy.get_include())')"
-LINKNAME="$PETSC_DIR/$PETSC_ARCH"
+LINKNAME="$PETSC_DIR/$PETSC_ARCH/include"
 
 echo "Numpy header files location: $NUMPY_INCLUDE"
 
@@ -13,4 +13,4 @@ if [[ -L "$LINKNAME" ]]; then
     unlink "$LINKNAME"
 fi
 
-ln -s "$NUMPY_INCLUDE/numpy" "$PETSC_DIR/$PETSC_ARCH/include"
+ln -s "$NUMPY_INCLUDE/numpy" "$LINKNAME"
