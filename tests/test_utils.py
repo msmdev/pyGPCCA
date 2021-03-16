de5f71726c1516fabad46d265f745a6439da7a43
Marius Lange <marius.lange@t-online.de>
2021-03-16 16:11:48 +0100

    Add regression tests for slepsc spectral decomposition:

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,25 @@
 import numpy as np
 
+from tests.conftest import assert_allclose, skip_if_no_petsc_slepc
+from pygpcca.utils._utils import _eigs_slepc
 
+
+@skip_if_no_petsc_slepc
 class TestEigendecompositionSLEPSc:
     def test_eigendecomposition_slepsc_regression(
         self,
         test_matrix_1: np.ndarray,
-        test_matrix_1_stationary_distribution,
+        test_matrix_1_eigenvalues: np.ndarray,
+        test_matrix_1_eigenvectors: np.ndarray,
         test_matrix_2: np.ndarray,
-        test_matrix_2_stationary_distribution,
-        test_matrix_3: np.ndarray,
+        test_matrix_2_eigenvalues: np.ndarray,
+        test_matrix_2_eigenvectors: np.ndarray,
     ):
 
-        pass
+        eigs_1, vecs_1 = _eigs_slepc(test_matrix_1, k=11)
+        assert_allclose(test_matrix_1_eigenvalues, eigs_1)
+        assert_allclose(test_matrix_1_eigenvectors, vecs_1)
+
+        eigs_2, vecs_2 = _eigs_slepc(test_matrix_2, k=13)
+        assert_allclose(test_matrix_2_eigenvalues, eigs_2)
+        assert_allclose(test_matrix_2_eigenvectors, vecs_2)
