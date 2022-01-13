494ce4e3709bbd103cf9536db5e25be928d3fe03
michalk8 <46717574+michalk8@users.noreply.github.com>
2022-01-13 00:09:22 +0000

    Patch release preparation (#35)
    * Bump numpy version, update CI pip cache
    
    * Try intalling newer PETSc/SLEPc versions on CI
    
    * Pin versions in tox.ini
    
    * Fix pins
    
    * Remove redundant CI step
    
    * Try remove arpack for macOS
    
    * Try using 3.9 on macOS
    
    * Temp. disable PETSc/SLEPc on macOS

--- .scripts/ci/install_dependencies.sh
+++ .scripts/ci/install_dependencies.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-function install_petsc_macos {
+function install_petsc {
     curl -O "https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-$PC_VERSION.tar.gz"
     tar -xzf "petsc-lite-$PC_VERSION.tar.gz"
     pushd "petsc-$PC_VERSION"
@@ -15,12 +15,12 @@ function install_petsc_macos {
     popd
 }
 
-function install_slepc_macos {
+function install_slepc {
     curl -O "https://slepc.upv.es/download/distrib/slepc-$SC_VERSION.tar.gz"
     tar -xzf "slepc-$SC_VERSION.tar.gz"
     pushd "slepc-$SC_VERSION"
 
-    ./configure --with-arpack-dir=/usr/local/Cellar/arpack
+    ./configure
     make all
     make check
     # make install  # only to move into the appropriate location
@@ -28,29 +28,28 @@ function install_slepc_macos {
     popd
 }
 
-
 if [[ "$RUNNER_OS" == "Linux" ]]; then
     echo "Installing PETSc/SLEPc dependencies for Linux"
     sudo apt-get update -y
-    sudo apt-get install gcc gfortran libopenmpi-dev libblas-dev liblapack-dev petsc-dev slepc-dev -y
+    sudo apt-get install gcc gfortran libopenmpi-dev libblas-dev liblapack-dev -y
 elif [[ "$RUNNER_OS" == "macOS" ]]; then
-    echo "Installing PETSc/SLEPc with SLEPc dir: '$SLEPC_DIR', PETSc dir: '$PETSC_DIR', PETSc arch: '$PETSC_ARCH'"
-    pushd "$HOME"
-
     brew install gcc open-mpi openblas lapack arpack
-    install_petsc_macos
-    install_slepc_macos
-
-    popd
-
-    # this seems to be only necessary on the CI
-    echo "Symlinking numpy"
-    python -m pip install --upgrade pip
-    pip install numpy
-
-    NUMPY_INCLUDE="$(python -c 'import numpy; print(numpy.get_include())')"
-    ln -sfv "$NUMPY_INCLUDE/numpy" "$PETSC_DIR/$PETSC_ARCH/include"
 else
-    echo "Invalid OS for PETSc/SLEPc dependencies: $OS"
+    echo "Invalid OS for PETSc/SLEPc dependencies: '$OS'"
     exit 42
 fi
+
+echo "Installing PETSc/SLEPc with SLEPc dir: '$SLEPC_DIR', PETSc dir: '$PETSC_DIR', PETSc arch: '$PETSC_ARCH'"
+
+pushd "$HOME"
+install_petsc
+install_slepc
+popd
+
+# this seems to be only necessary on the CI
+echo "Installing and symlinking numpy"
+python -m pip install --upgrade pip
+pip install numpy
+
+NUMPY_INCLUDE="$(python -c 'import numpy; print(numpy.get_include())')"
+ln -sfv "$NUMPY_INCLUDE/numpy" "$PETSC_DIR/$PETSC_ARCH/include"
