a1a59d9aecdef52027aa1ad6432499bbe220a778
michalk8 <46717574+michalk8@users.noreply.github.com>
2022-02-08 20:14:16 +0000

    Fix intersphinx numpy/scipy (#37)
    * Fix intersphinx numpy/scipy
    
    * More general link for scipy
    
    Co-authored-by: Bernhard Reuter <43750470+msmdev@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,8 +52,8 @@ extensions = [
 ]
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
 }
 
 # Add any paths that contain templates here, relative to this directory.
