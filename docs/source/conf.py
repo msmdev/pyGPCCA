ca0895c28ffe1f28eb1b19e895b40e0bb6a1f1a1
Michal Klein <46717574+michalk8@users.noreply.github.com>
2023-06-01 00:07:42 +0200

    Fix linkcheck 403

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -115,7 +115,11 @@ spelling_filters = ["enchant.tokenize.URLFilter", "enchant.tokenize.EmailFilter"
 
 # linkcheck
 linkcheck_anchors = False  # problem with specifying lines on GitHub in `acknowledgments.rst`
-linkcheck_ignore = ["https://doi.org/10.1021/acs.jctc.8b00079", "https://pubs.acs.org/doi/abs/10.1021/acs.jctc.8b00079"]
+linkcheck_ignore = [
+    "https://doi.org/10.1021/acs.jctc.8b00079",
+    "https://pubs.acs.org/doi/abs/10.1021/acs.jctc.8b00079",
+    "https://doi.org/10.1063/1.5064530",
+]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
